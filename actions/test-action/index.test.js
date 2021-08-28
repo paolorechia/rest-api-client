36654afd7681a7a608d1d69bef5b9fac6061a1c9
Paolo Rechia <paolorechia@gmail.com>
2021-08-27 22:13:12 -0300

    Test workflow

---
+++ actions/test-action/index.test.js
@@ -0,0 +1,11 @@
+const wait = require('./wait');
+const process = require('process');
+const cp = require('child_process');
+const path = require('path');
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  process.env['TOX_ENV'] = "py38-gh-unit";
+  const ip = path.join(__dirname, 'index.js');
+  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+})
